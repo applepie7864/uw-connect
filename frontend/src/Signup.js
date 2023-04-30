1a6f616cb643f4183cf6e7effd6f58a576edb48c
Annie Guo <anniewguo@Annies-MacBook-Air-2.local>
2023-04-30 16:48:45 -0400

    added bg

--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -2,18 +2,20 @@ import React from 'react'
 import './Signup.css'
 import { gsap } from "gsap";
 import { Link } from 'react-router-dom';
+import videoBg from './whitebg-video.mp4'
 
 function Signup() {
-  return (
-    <div className='main-container'>
-      <div className='left-side'>
-        
-      </div>
-      <div className="right-side">
+    return (
+        <div className='main-container'>
+        <div className='left-side'>
 
-      </div>
-    </div>
-  )
+        </div>
+        <div className="right-side">
+
+        </div>
+        <video id="intro-video" src={videoBg} autoPlay muted loop ></video>
+        </div>
+    )
 }
 
 export default Signup
